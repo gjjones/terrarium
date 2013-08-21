ded39776eb080598849538b708dc579e84fbbf7d
gjjones <gjjones@outlook.com>
2013-08-21 15:45:24 -0400

    Adds Dictionary type and directions object.

--- terrarium.js
+++ terrarium.js
@@ -1,3 +1,29 @@
+function forEachIn(object, action) {
+	for (var property in object) {
+		if (Object.prototype.hasOwnProperty.call(object, property))
+			action(property, object[property]);
+	}
+}
+
+function Dictionary(startValues) {
+	this.values = startValues || {};
+}
+Dictionary.prototype.store = function(names, values) {
+	this.values[name] = value;
+};
+Dictionary.prototype.lookup = function(name) {
+	return this.values[name];
+};
+Dictionary.prototype.contains = function(name) {
+	return Object.prototype.hasOwnProperty.call(this.values, name) &&
+			Object.prototype.propertyIsEnumerable.call(this.values, name);
+};
+Dictionary.prototype.each = function(action) {
+	forEachIn(this.values, action);
+};
+
+
+
 var thePlan =
   ["############################",
    "#      #    #      o      ##",
@@ -57,9 +83,23 @@ Grid.prototype.each = function(action) {
 	}
 };
 
-var testGrid = new Grid(3, 2);
-testGrid.setValueAt(new Point(1, 0), "#");
-testGrid.setValueAt(new Point(1, 1), "o");
-testGrid.each(function(point, value) {
-	console.log([point.x, ", ", point.y, ": ", value].join(""));
+// var testGrid = new Grid(3, 2);
+// testGrid.setValueAt(new Point(1, 0), "#");
+// testGrid.setValueAt(new Point(1, 1), "o");
+// testGrid.each(function(point, value) {
+// 	console.log([point.x, ", ", point.y, ": ", value].join(""));
+// });
+
+
+var directions = new Dictionary({
+	"n":  new Point( 0, -1),
+	"ne": new Point( 1, -1),
+	"e":  new Point( 1,  0),
+	"se": new Point( 1,  1),
+	"s":  new Point( 0,  1),
+	"sw": new Point(-1,  1),
+	"w":  new Point(-1,  0),
+	"nw": new Point(-1, -1),
 });
+
+//console.log(new Point(4, 4).add(directions.lookup("se")));
