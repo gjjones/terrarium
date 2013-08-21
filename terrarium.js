6d0bdf7713102466ba1579a019be019ee7e5f68a
gjjones <gjjones@outlook.com>
2013-08-21 11:42:16 -0400

    adding the main script file for the code.

---
+++ terrarium.js
@@ -0,0 +1,27 @@
+var thePlan =
+  ["############################",
+   "#      #    #      o      ##",
+   "#                          #",
+   "#          #####           #",
+   "##         #   #    ##     #",
+   "###           ##     #     #",
+   "#           ###      #     #",
+   "#   ####                   #",
+   "#   ##       o             #",
+   "# o  #         o       ### #",
+   "#    #                     #",
+   "############################"];
+
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
+Point.prototype.add = function(other) {
+	return new Point(this.x + other.x, this.y + other.y);
+};
+Point.prototype.isEqualTo = function(other) {
+	return this.x === other.x && this.y === other.y;
+};
+
+// console.log(new Point(1, 4).add(new Point(4, 1)));
+
